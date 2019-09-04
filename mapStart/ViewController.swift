cf2869498f4bf3f63184a0660a121d711c6a2a3b
nathanielmcdowell <nathaniel29651@gmail.com>
2019-09-04 17:45:18 -0400

    Initial Commit

---
+++ mapStart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mapStart
+//
+//  Created by Nathaniel Mcdowell on 9/4/19.
+//  Copyright © 2019 Nathaniel Mcdowell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
